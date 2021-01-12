1641103cb021797b58608ac31bf8e66006432af2
waseem shaikh <b.waseem.403@gmail.com>
2021-01-13 00:23:29 +0530

    version 1.1

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get("/Users", (req, res) => {
   res.json(Users);
 });
 
+app.post("/Register_Users",(req,res)=>{
+    res.send('user is regsiter successfully...')
+})
+
 const port = process.env.PORT || 5050;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
